c76358fd69314e4508e1ce7bca7ad7297b1fc3f6
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-10-01 16:27:51 +0200

    Initial Commit

---
+++ components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,7 @@
+const ProfileCard = () => {
+  return (
+    <div className="absolute inset-0 m-auto h-[420px] w-[320px] rounded-[12px] bg-transparent bg-gradient-to-r from-[rgba(245,141,228,0.3)] to-[rgba(231,12,195,0.2)] shadow-[0_0px_80px_0_rgba(85,127,173,0.37)] backdrop-blur-[20px]"></div>
+  );
+};
+
+export default ProfileCard;
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import ProfileCard from "../components/ProfileCard/ProfileCard";
+
+function App() {
+  return <div className="card"></div>;
+}
+
+export default App;
